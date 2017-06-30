059296418d3e2e8656c9714b33466a8ffa65a73c
User <User@DESK-CGZ7J32.lexicon.edu.local>
2017-06-30 15:28:25 +0200

    changed layout

--- Project1/src/GUI.java
+++ Project1/src/GUI.java
@@ -14,7 +14,7 @@ import javafx.scene.control.MultipleSelectionModel;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -29,15 +29,14 @@ public class GUI extends Application {
 
 		myStage.setTitle("Todo list");
 		initButtons();
-		
-		todoItems = FXCollections.observableArrayList(
-				new TodoItem("äta lunch", 3), new TodoItem("äta middag", 5), new TodoItem("sova", 1), new TodoItem("städa", 10));
 
-        table.setEditable(true);
-        createTableColumns();
-        table.setItems(todoItems);
-		
-		
+		todoItems = FXCollections.observableArrayList(new TodoItem("äta lunch", 3), new TodoItem("äta middag", 5),
+				new TodoItem("sova", 1), new TodoItem("städa", 10));
+
+		table.setEditable(true);
+		createTableColumns();
+		table.setItems(todoItems);
+
 		MultipleSelectionModel<TodoItem> tableViewSelModel = table.getSelectionModel();
 		tableViewSelModel.selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
 			public void changed(ObservableValue<? extends TodoItem> changed, TodoItem oldVal, TodoItem newVal) {
@@ -47,31 +46,36 @@ public class GUI extends Application {
 
 		VBox buttonSelection = new VBox();
 		buttonSelection.setSpacing(10);
+		buttonSelection.setAlignment(Pos.TOP_RIGHT);
 		buttonSelection.getChildren().addAll(addButton, editButton, deleteButton, deleteAllDoneButton, doneButton, undoneButton);
+		
+		BorderPane borderPane = new BorderPane();
+		borderPane.setPadding(new Insets(10,10,10,10));
+		borderPane.setCenter(table);
+		borderPane.setRight(buttonSelection);
+		BorderPane.setMargin(buttonSelection, new Insets(10,10,10,10));
 
-		GridPane pane = new GridPane();
-		pane.setHgap(10);
-		pane.setVgap(20);
-		pane.setAlignment(Pos.CENTER);
-
-		pane.addColumn(0, table);
-		pane.addColumn(1, buttonSelection);
-		pane.setPadding(new Insets(10,10,10,10));
-
-		Scene myScene = new Scene(pane, 500, 300);
+		Scene myScene = new Scene(borderPane, 500, 300);
 		myStage.setScene(myScene);
 		myStage.show();
 	}
-	
-	private void initButtons(){
+
+	private void initButtons() {
 		addButton = new Button("Add");
 		editButton = new Button("Edit");
 		doneButton = new Button("Done");
 		undoneButton = new Button("Undone");
 		deleteButton = new Button("Delete");
 		deleteAllDoneButton = new Button("Delete done");
-		
-        addButton.setOnAction(new EventHandler<ActionEvent>() {
+
+		addButton.setPrefWidth(100);
+		editButton.setPrefWidth(100);
+		doneButton.setPrefWidth(100);
+		undoneButton.setPrefWidth(100);
+		deleteButton.setPrefWidth(100);
+		deleteAllDoneButton.setPrefWidth(100);
+
+		addButton.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent ae) {
 				addItem();
 			}
@@ -83,13 +87,15 @@ public class GUI extends Application {
 		});
         doneButton.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent ae) {
-				selectedItem.setDone();
+				if (selectedItem != null)
+					selectedItem.setDone();
 				table.refresh();
 			}
 		});
 		undoneButton.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent ae) {
-				selectedItem.setUnDone();
+				if (selectedItem != null)
+					selectedItem.setUnDone();
 				table.refresh();
 			}
 		});
@@ -104,42 +110,45 @@ public class GUI extends Application {
 			}
 		});
 	}
-	
-	private void createTableColumns() {
-        TableColumn<TodoItem, String> descriptionColumn = new TableColumn<TodoItem, String>("Description");
-        descriptionColumn.setMinWidth(100);
-        descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
- 
-        TableColumn<TodoItem, String> dueDateColumn = new TableColumn<TodoItem, String>("Due date");
-        dueDateColumn.setMinWidth(100);
-        dueDateColumn.setCellValueFactory(new PropertyValueFactory<>("dateDue"));
-        
-        TableColumn<TodoItem, String> dateAddedColumn = new TableColumn<TodoItem, String>("Date added");
-        dateAddedColumn.setMinWidth(100);
-        dateAddedColumn.setCellValueFactory(new PropertyValueFactory<>("dateAdded"));
-        
-        TableColumn<TodoItem, String> completedColumn = new TableColumn<TodoItem, String>("Done");
-        completedColumn.setMinWidth(50);
-        completedColumn.setCellValueFactory(new PropertyValueFactory<>("statusString"));
 
-        table.getColumns().addAll(descriptionColumn, dueDateColumn, dateAddedColumn, completedColumn);
+	private void createTableColumns() {
+		TableColumn<TodoItem, String> descriptionColumn = new TableColumn<TodoItem, String>("Description");
+		descriptionColumn.setMinWidth(100);
+		descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+
+		TableColumn<TodoItem, String> dueDateColumn = new TableColumn<TodoItem, String>("Due date");
+		dueDateColumn.setMinWidth(90);
+		dueDateColumn.setCellValueFactory(new PropertyValueFactory<>("dateDue"));
+
+		TableColumn<TodoItem, String> dateAddedColumn = new TableColumn<TodoItem, String>("Date added");
+		dateAddedColumn.setMinWidth(90);
+		dateAddedColumn.setCellValueFactory(new PropertyValueFactory<>("dateAdded"));
+
+		TableColumn<TodoItem, String> completedColumn = new TableColumn<TodoItem, String>("Done");
+		completedColumn.setMinWidth(80);
+		completedColumn.setCellValueFactory(new PropertyValueFactory<>("statusString"));
+
+		table.getColumns().addAll(descriptionColumn, dueDateColumn, dateAddedColumn, completedColumn);
 	}
-	
-	private void addItem(){
+
+	private void addItem() {
 		ItemControllerWindow wc = new ItemControllerWindow();
-        wc.showStage("Add item", "");
-		todoItems.add(new TodoItem(wc.getDescription(), wc.getDays()));
+		wc.showStage("Add item", "");
+		if (wc.getComplete())
+			todoItems.add(new TodoItem(wc.getDescription(), wc.getDays()));
 	}
-	
+
 	private void editItem() {
 		ItemControllerWindow wc = new ItemControllerWindow();
-        wc.showStage("Edit item", selectedItem.getDescription());
-		selectedItem.setDescription(wc.getDescription());
-		selectedItem.setDates(wc.getDays(), false);
+		wc.showStage("Edit item", selectedItem.getDescription());
+		if (wc.getComplete()) {
+			selectedItem.setDescription(wc.getDescription());
+			selectedItem.setDates(wc.getDays(), false);
+		}
 		table.refresh();
 	}
-	
-	private void deleteAllDone(){
+
+	private void deleteAllDone() {
 		for (Iterator<TodoItem> iterator = todoItems.iterator(); iterator.hasNext();) {
 			TodoItem ti = iterator.next();
 			if (ti.getStatus())
@@ -150,5 +159,5 @@ public class GUI extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 }
--- Project1/src/ItemControllerWindow.java
+++ Project1/src/ItemControllerWindow.java
@@ -1,8 +1,12 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -10,18 +14,24 @@ import javafx.stage.Stage;
 class ItemControllerWindow {
 	private String description;
 	private int days;
+	private boolean complete;
 
 	void showStage(String message, String oldDescription) {
+		
+		complete = false;
 		Stage stage = new Stage();
 		stage.setTitle(message);
 		stage.initModality(Modality.APPLICATION_MODAL);
 
-		VBox root = new VBox();
-		root.setPadding(new Insets(10, 10, 10, 10));
-		root.setSpacing(5);
-		root.setMinWidth(250);
+		VBox vBox = new VBox();
+		HBox hBox = new HBox();
+		vBox.setPadding(new Insets(10, 10, 10, 10));
+		vBox.setSpacing(5);
+		vBox.setMinWidth(250);
+		hBox.setSpacing(5);
+		hBox.setAlignment(Pos.BOTTOM_RIGHT);
 
-		Scene scene = new Scene(root);
+		Scene scene = new Scene(vBox);
 		Label lableDescription = new Label("Description:");
 		TextField tfDescription = new TextField();
 		tfDescription.setText(oldDescription);
@@ -29,28 +39,41 @@ class ItemControllerWindow {
 		Label lableDays = new Label("Days to done:");
 		TextField tfDays = new TextField();
 
-		Button add = new Button("Submit");
+		Button addButton = new Button("Submit");
+		Button cancelButton = new Button("Cancel");
 
-		add.setOnAction(e -> {
+		addButton.setOnAction(e -> {
 			description = tfDescription.getText();
 			try {
 				days = Integer.parseInt(tfDays.getText());
+				complete = true;
 			} catch (NumberFormatException ne) {
 				System.out.println("Not a number");
 			}
 			stage.close();
 		});
+		cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent ae) {
+				stage.close();
+			}
+		});
 
-		root.getChildren().addAll(lableDescription, tfDescription, lableDays, tfDays, add);
+		hBox.getChildren().addAll(addButton, cancelButton);
+		vBox.getChildren().addAll(lableDescription, tfDescription, lableDays, tfDays, hBox);
 		stage.setScene(scene);
 		stage.showAndWait();
 	}
 
-	int getDays() {
+	public int getDays() {
 		return days;
 	}
 
-	String getDescription() {
+	public String getDescription() {
 		return description;
 	}
+	
+	public boolean getComplete(){
+		return complete;
+	}
+	
 }
